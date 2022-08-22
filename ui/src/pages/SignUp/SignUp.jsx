96146272055ec957a445434580f154833adc6062
jaballogian <jaballogianrma77@gmail.com>
2022-08-22 17:58:06 +0700

    add title Typography and company email Form Control components

--- ui/src/pages/SignUp/SignUp.jsx
+++ ui/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,71 @@
+import { useState } from 'react'
+
+// MUIS
+import FormControl from '@mui/material/FormControl'
+import FormHelperText from '@mui/material/FormHelperText'
+import InputLabel from '@mui/material/InputLabel'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import Typography from '@mui/material/Typography'
+
+// STYLES
+import useLayoutStyles from 'styles/layoutAuthenticationHalf'
+
 const SignUp = () => {
+  const layoutClasses = useLayoutStyles()
+
+  const initialFormObject = {
+    companyEmail: '',
+  }
+
+  const initialFormHelperObject = {
+    companyEmail: null,
+  }
+
+  const [ formObject, setFormObject ] = useState(initialFormObject)
+  const [ formHelperObject, setFormHelperObject ] = useState(initialFormHelperObject)
+
+  const handleFormObjectChange = (inputEvent) => {
+    setFormObject(current => {
+      return {
+        ...current,
+        [inputEvent.target.name]: inputEvent.target.value,
+      }
+    })
+  }
+
   return (
-    <div>SignUp</div>
+    <>
+      {/* TITLE */}
+      <Typography
+        variant='h6'
+        className={layoutClasses.textTitle}
+      >
+        Let's Get Started
+      </Typography>
+
+      {/* COMPANY EMAIL FORM */}
+      <FormControl 
+        variant='outlined' 
+        fullWidth
+      >
+        <InputLabel>
+          Company Email
+        </InputLabel>
+        
+        <OutlinedInput
+          name='companyEmail'
+          type='email'
+          value={formObject.companyEmail}
+          onChange={handleFormObjectChange}
+          label='Company Email'
+        />
+
+        {formHelperObject.companyEmail &&
+        <FormHelperText>
+          {formHelperObject.companyEmail}
+        </FormHelperText>}
+      </FormControl>
+    </>
   )
 }
 
