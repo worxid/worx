d5a0feb5a914115e8e65e9fb39ef952762bfab6f
jaballogian <jaballogianrma77@gmail.com>
2022-08-22 14:42:33 +0700

    change the favicon and title of the app

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,8 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+
+// ASSETS
+import LogoFavicon from 'assets/images/logos/favicon.png'
 
 // COMPONENTS
 import AuthenticationRoute from 'components/Routes/AuthenticationRoute'
@@ -25,6 +29,12 @@ const App = () => {
     }
     else if (inputItem.routeType === 'free') return inputItem.element
   }
+
+  // CHANGE THE FAVICON
+  useEffect(() => {
+    const faviconElement = document.getElementById('favicon')
+    faviconElement.href = LogoFavicon
+  }, [])
 
   return (
     <Routes>
