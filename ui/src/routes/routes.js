ef3b5158ba4e5fbc2694af04a336c2731451c69a
jaballogian <jaballogianrma77@gmail.com>
2022-08-22 17:33:41 +0700

    add Sign Up page

--- ui/src/routes/routes.js
+++ ui/src/routes/routes.js
@@ -1,12 +1,19 @@
 // PAGES
 import Home from 'pages/Home/Home'
 import SignIn from 'pages/SignIn/SignIn'
+import SignUp from 'pages/SignUp/SignUp'
 
 const routes = [
   // AUTHENTICATION
   {
     path: '/sign-in',
     element: <SignIn/>,
+    routeType: 'authentication',
+    authenticationType: 'half',
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp/>,
     routeType: 'authentication',
     authenticationType: 'half',
   },
