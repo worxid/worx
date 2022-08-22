1972ff78ca07821f26b68301496aa2f598a8c8c2
jaballogian <jaballogianrma77@gmail.com>
2022-08-22 17:57:30 +0700

    add layout authentication half style

---
+++ ui/src/styles/layoutAuthenticationHalf.js
@@ -0,0 +1,15 @@
+// CONSTANTS
+import { values } from 'constants/values'
+
+// MUI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  textTitle: {
+    fontWeight: 700,
+    fontFamily: values.fontFamilySpaceMono,
+    marginBottom: 40,
+  },
+}))
+
+export default useStyles
