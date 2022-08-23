6a5f6021f37244b44f6929c8648d5dd93ea278a4
jaballogian <jaballogianrma77@gmail.com>
2022-08-23 14:41:15 +0700

    add new style

--- ui/src/styles/layoutAuthenticationHalf.js
+++ ui/src/styles/layoutAuthenticationHalf.js
@@ -7,12 +7,19 @@ import { makeStyles } from '@mui/styles'
 const useStyles = makeStyles((theme) => ({
   form: {
     width: '100%',
-    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   textTitle: {
     fontWeight: 700,
     fontFamily: values.fontFamilySpaceMono,
     marginBottom: 40,
+  },
+  textLink: {
+    alignSelf: 'flex-end',
+    fontWeight: 700,
+    fontFamily: values.fontFamilySpaceMono,
   },
   buttonAction: {
     margin: '24px 0px',
