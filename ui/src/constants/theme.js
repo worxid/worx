e9f0e35eeef05a7af7c4752fd6d9b1a59c9314ab
jaballogian <jaballogianrma77@gmail.com>
2022-08-25 17:13:28 +0700

    add additional colors

--- ui/src/constants/theme.js
+++ ui/src/constants/theme.js
@@ -14,6 +14,7 @@ let customTheme = createTheme({
     warning: colors.warning,
     success: colors.success,
     text: colors.text,
+    additional: colors.additional,
   },
   typography: {
     fontFamily: values.fontFamilyDmMono,
