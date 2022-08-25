ca0ebfce51ccbb486bb2ed9179c075dc7477897b
jaballogian <jaballogianrma77@gmail.com>
2022-08-25 16:20:47 +0700

    add some components and their styles

--- ui/src/layouts/Private/Private.jsx
+++ ui/src/layouts/Private/Private.jsx
@@ -1,10 +1,36 @@
+// COMPONENTS
+import Drawer from 'components/Drawer/Drawer'
+
+// MUIS
+import CssBaseline from '@mui/material/CssBaseline'
+import Stack from '@mui/material/Stack'
+
+// STYLES
+import useStyles from './privateUseStyles'
+
 const Private = (props) => {
   const { children } = props
 
+  const classes = useStyles()
+
   return (
-    <div>
-      {children}
-    </div>
+    <Stack 
+      direction='row'
+      className={`${classes.root} no-zoom`}
+    >
+      <CssBaseline/>
+
+      {/* DRAWER */}
+      <Drawer/>
+
+      {/* CONTENT CONTAINER */}
+      <Stack
+        component='main'
+        className={`${classes.contentContainer} zoom`}
+      >
+        {children}
+      </Stack>
+    </Stack>
   )
 }
 
