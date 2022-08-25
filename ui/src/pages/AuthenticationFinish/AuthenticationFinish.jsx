ccba160c8632c2442e5a6fe2e1ac2b4863b46e03
jaballogian <jaballogianrma77@gmail.com>
2022-08-25 10:53:22 +0700

    add get information function

--- ui/src/pages/AuthenticationFinish/AuthenticationFinish.jsx
+++ ui/src/pages/AuthenticationFinish/AuthenticationFinish.jsx
@@ -1,4 +1,7 @@
+import { useSearchParams } from 'react-router-dom'
+
 // ASSETS
+import IconCheck from 'assets/images/icons/authentication-check.svg'
 import IconEmail from 'assets/images/icons/authentication-email.svg'
 
 // MUIS
@@ -13,12 +16,45 @@ import useStyles from './authenticationFinishUseStyles'
 const AuthenticationFinish = () => {
   const classes = useStyles()
 
+  const [ searchParams ] = useSearchParams()
+
+  const type = searchParams.get('type')
+  const email = searchParams.get('email')
+
+  const getinformation = (inputType) => {
+    let informationObject = {}
+
+    if (inputType === 'sign-up' || inputType === 'forgot-password') {
+      informationObject = {
+        title: 'Check Your Email',
+        icon: IconEmail,
+      }
+
+      if (inputType === 'sign-up') {
+        informationObject.caption1 = 'Click the link in your email'
+        informationObject.caption2 = 'to activate your account'
+      }
+      else if (inputType === 'forgot-password') {
+        informationObject.caption1 = 'A link to reset your password'
+        informationObject.caption2 = 'has been sent to your email'
+      }
+    }
+    else if (inputType === 'reset-password') {
+      informationObject = {
+        title: 'Your password has been changed successfully',
+        icon: IconCheck,
+      }
+    }
+
+    return informationObject
+  }
+
   return (
     <>
       {/* EMAIL ICON */}
       <Box
         component='img'
-        src={IconEmail}
+        src={getinformation(type).icon}
         alt=''
         className={classes.icon}
       />
@@ -28,14 +64,14 @@ const AuthenticationFinish = () => {
         variant='h6'
         className={classes.textTitle}
       >
-        Check Your Email
+        {getinformation(type).title}
       </Typography>
 
       {/* CAPTION */}
       <Typography variant='subtitle1'>
-        Click the link in your email
+        {getinformation(type).caption1}
         <br/>
-        to activate your account
+        {getinformation(type).caption2}
       </Typography>
 
       {/* SIGN IN BUTTON */}
@@ -51,6 +87,7 @@ const AuthenticationFinish = () => {
       </Button>
 
       {/* RESEND TEXT */}
+      {(type === 'sign-up' || type === 'forgot-password') &&
       <Typography variant='subtitle1'>
         Didn’t receive the link?&nbsp;
         <Link
@@ -59,7 +96,7 @@ const AuthenticationFinish = () => {
         >
           Resend
         </Link>
-      </Typography>
+      </Typography>}
     </>
   )
 }
