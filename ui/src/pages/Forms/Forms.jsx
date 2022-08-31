89b69fb529dfd4471df0a3e30766514b05b630ce
jaballogian <jaballogianrma77@gmail.com>
2022-08-31 13:27:06 +0700

    add App Bar component

--- ui/src/pages/Forms/Forms.jsx
+++ ui/src/pages/Forms/Forms.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+
 // COMPONENTS
 import AppBar from 'components/AppBar/AppBar'
 
 const Forms = () => {
+  const [ pageSearch, setPageSearch ] = useState('')
+
   return (
     <>
-      <AppBar/>
+      <AppBar
+        hasFab={true}
+        pageTitle='Forms'
+        hasSearch={true}
+        search={pageSearch}
+        setSearch={setPageSearch}
+      />
     </>
   )
 }
