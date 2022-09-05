e93d6b8117d0e8fd5b11327fc80d15cf402cb2f1
dekguh <kdkteguhmahesa@gmail.com>
2022-09-05 11:50:10 +0800

    create InputComponent & ItemGrid

---
+++ ui/src/pages/FormsView/ItemGrid/ItemGrid.jsx
@@ -0,0 +1,41 @@
+// MUIS
+import Divider from '@mui/material/Divider'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+// STYLES
+import useStyles from '../formsViewUseStyles'
+
+const ItemGrid = (props) => {
+  const { label, description, isSeparator, children } = props
+
+  // STYLES
+  const classes = useStyles()
+
+  return (
+    <>
+      {isSeparator && (
+        <Grid item xs={12} className={classes.gridItemSeparator}>
+          <Divider/>
+        </Grid>
+      )}
+
+      <Grid item xs={4}>
+        <Typography variant='subtitle2' >{label}</Typography>
+
+        <Typography
+          variant='caption'
+          color='text.secondary'
+        >
+          {description}
+        </Typography>
+      </Grid>
+
+      <Grid item xs={8}>
+        {children}
+      </Grid>
+    </>
+  )
+}
+
+export default ItemGrid
