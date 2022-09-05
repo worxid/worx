d17ec059f91f8369261acc94c8b5f5140d38d9a5
dekguh <kdkteguhmahesa@gmail.com>
2022-09-05 11:50:39 +0800

    create forms view page

---
+++ ui/src/pages/FormsView/FormsView.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+// COMPONENTS
+import AppBar from 'components/AppBar/AppBar'
+import Header from './Header/Header'
+import ItemGrid from './ItemGrid/ItemGrid'
+import LoadingPaper from 'components/LoadingPaper/LoadingPaper'
+
+// CONSTANTS
+import { dummyData } from './formsViewConstants'
+
+// MUIS
+import Divider from '@mui/material/Divider'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+
+// STYLES
+import useStyles from './formsViewUseStyles'
+import InputComponent from './InputComponent/InputComponent'
+
+const FormsView = () => {
+  // STYLES
+  const classes = useStyles()
+
+  // CONTENT
+  const [ isDataGridLoading, setIsDataGridLoading ] = useState(false)
+
+  return (
+    <>
+      <AppBar
+        hasFab={false}
+        hasBack={true}
+        backLink='/forms'
+        pageTitle='Form View'
+        hasSearch={false}
+      />
+
+      {/* CONTENTS */}
+      <Stack
+        direction='row'
+        position='relative'
+        flex='1'
+        height='100%'
+        className={classes.contents}
+      >
+        {/* MAIN CONTENT */}
+        <LoadingPaper isLoading={isDataGridLoading}>
+          {/* HEADER */}
+          <Header title='Valid Form'/>
+
+          <Divider />
+
+          {/* CONTENT FORM */}
+          <Grid container spacing={0} className={classes.contentForms}>
+            {dummyData.value.fields.map((item, index) => (
+              <ItemGrid
+                label={item.label}
+                description={item.description}
+                key={index}
+                isSeparator={item.type === 'separator'}
+              >
+                <InputComponent
+                  type={item.type}
+                />
+              </ItemGrid>
+            ))}
+            {/* ITEM */}
+          </Grid>
+        </LoadingPaper>
+      </Stack>
+    </>
+  )
+}
+
+export default FormsView
---
+++ ui/src/pages/FormsView/Header/Header.jsx
@@ -0,0 +1,40 @@
+// MUIS
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+
+// MUI ICONS
+import IconDownload from '@mui/icons-material/Download'
+
+// STYLES
+import useStyles from '../formsViewUseStyles'
+
+const Header = (props) => {
+  const { title } = props
+
+  // STYLES
+  const classes = useStyles()
+
+  return (
+    <Stack
+      className={classes.header}
+      direction='row'
+      alignItems='center'
+    >
+      {/* TITLE */}
+      <Typography
+        className={classes.headerTitle}
+        variant='subtitle1'
+      >
+        {title}
+      </Typography>
+
+      {/* BUTTON DOWNLOAD */}
+      <IconButton>
+        <IconDownload />
+      </IconButton>
+    </Stack>
+  )
+}
+
+export default Header
