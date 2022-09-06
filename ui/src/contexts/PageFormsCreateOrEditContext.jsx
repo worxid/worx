c4fd4edbe507b0fc060afa7cd4b81ff36f87c460
dekguh <kdkteguhmahesa@gmail.com>
2022-09-06 12:40:23 +0800

    create context

---
+++ ui/src/contexts/PageFormsCreateOrEditContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from 'react'
+
+// CONSTANTS
+import { dataListComponents } from 'pages/FormsCreateOrEdit/formsCreateOrEditConstants'
+
+const PageFormsCreateOrEditContext = createContext()
+
+const PageFormsCreateOrEditContextProvider = (props) => {
+  // BOOLEAN
+  const [isFormLoading, setIsFormLoading] = useState(false)
+  // DRAG AND DROP
+  const [listFields, setListFields] = useState([])
+  const [listToolbox, setListToolbox] = useState(dataListComponents)
+  const [selectedFields, setSelectedFields] = useState()
+
+  return (
+    <PageFormsCreateOrEditContext.Provider
+      value={{
+        // BOOLEAN
+        isFormLoading, setIsFormLoading,
+        // DRAG AND DROP
+        listFields, setListFields,
+        listToolbox, setListToolbox,
+        selectedFields, setSelectedFields
+      }}
+    >
+      {props.children}
+    </PageFormsCreateOrEditContext.Provider>
+  )
+}
+
+export { PageFormsCreateOrEditContextProvider, PageFormsCreateOrEditContext }
