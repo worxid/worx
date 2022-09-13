33b15c278acc2e60ce71364dd64fe7af8a2a43f2
laneeeyyy <60956215+laneeeyyy@users.noreply.github.com>
2022-09-13 16:22:00 +0700

    Frontend features/page settings group (#43)
    * Features Page Settings Group
    
    * Changr wording device into group
    
    * change zIndex value at dialogAddOrEditUseStyles
    
    * Adjust styling variables

---
+++ ui/src/pages/SettingsGroup/settingsGroupConstants.js
@@ -0,0 +1,37 @@
+export const dummyTableData = [
+  {
+    'id': 1,
+    'groupName': 'Dominique Hoover',
+    'groupColor': '#FDF184',
+    'totalDevices': 8,
+    'totalForm': 4
+  },
+  {
+    'id': 2,
+    'groupName': 'Rhoda Blankenship',
+    'groupColor': '#D87972',
+    'totalDevices': 2,
+    'totalForm': 8
+  },
+  {
+    'id': 3,
+    'groupName': 'Hedley Mckay',
+    'groupColor': '#667C8A',
+    'totalDevices': 5,
+    'totalForm': 7
+  },
+  {
+    'id': 4,
+    'groupName': 'Katell Wall',
+    'groupColor': '#E26C92',
+    'totalDevices': 9,
+    'totalForm': 2
+  },
+  {
+    'id': 5,
+    'groupName': 'Julian Gomez',
+    'groupColor': '#FCE95E',
+    'totalDevices': 3,
+    'totalForm': 6
+  }
+]
