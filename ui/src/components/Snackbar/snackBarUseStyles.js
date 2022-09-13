0dd8e29c8490bdd9b6bb0ef6043e782e5334a1f2
Lani Diana <ganeshabook2015@gmail.com>
2022-09-13 10:47:42 +0700

    Change background color

--- ui/src/components/Snackbar/snackBarUseStyles.js
+++ ui/src/components/Snackbar/snackBarUseStyles.js
@@ -1,12 +1,9 @@
-// CONSTANT
-import {colors} from 'constants/colors'
-
 // MUI STYLES
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
   alertContainer: {
-    backgroundColor: colors.background.paper,
+    backgroundColor: theme.palette.background.paper,
     borderRadius: 4,
     borderWidth: 1.5,
     color: theme.palette.text.primary
