e1e918f32c88be24eee741298c7319ff3c367509
Jabal Logian <jaballogianrma77@gmail.com>
2022-09-28 14:01:54 +0700

    Revisions/layout authentication (#62)
    * change the components and styles
    
    * replace text and link Typography component with App Bar Text component
    
    * revise the page based on the newest design
    
    * revise the page based on the newest design
    
    * revise the page based on the newest design
    
    * delete unused style
    
    * delete type prop
    
    * revise the page based on the newest design
    
    * add env file to the git ignore
    
    * add env example
    
    * add new condition for the returned component
    
    * update href props

--- ui/src/layouts/Authentication/Authentication.jsx
+++ ui/src/layouts/Authentication/Authentication.jsx
@@ -1,12 +1,15 @@
 // ASSETS
 import LogoProductWithText from 'assets/images/logos/product-logo-with-text.svg'
-import PictureComplementary from 'assets/images/pictures/authentication-complementary.svg'
+
+// COMPONENTS
+import AppBarText from './AppBarText'
 
 // MUIS
+import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
+import Toolbar from '@mui/material/Toolbar'
 
 // STYLES
 import useStyles from './authenticationUseStyles'
@@ -17,65 +20,39 @@ const AuthenticationHalf = (props) => {
   const classes = useStyles()
 
   return (
-    <Grid 
-      container
-      className={`${classes.root} no-zoom`}
-    >
-      {/* SIDE CONTENT */}
-      <Grid
-        item
-        xs={6}
-        className={`${classes.content} ${classes.contentSide} zoom`}
+    <Stack className={`${classes.root} no-zoom`}>
+      {/* APP BAR */}
+      <AppBar 
+        className={`${classes.appBar} zoom`}
+        position='sticky'
       >
-        <Stack className={classes.containerText}>
-          {/* TITLE */}
-          <Typography
-            variant='h5'
-            className={`${classes.text} ${classes.textTitle}`}
-          >
-            Mobile Form System For Professional Field Workers
-          </Typography>
+        <Toolbar className={classes.toolbar}>
+          {/* PRODUCT LOGO */}
+          <Link href='/'>
+            <Box
+              component='img'
+              src={LogoProductWithText}
+              alt=''
+            />
+          </Link>
 
-          {/* CAPTION */}
-          <Typography
-            variant='subtitle1'
-            className={classes.text}
-          >
-            A hassle-free mobile data collection and workforce management for any business and professional teams.
-          </Typography>
-        </Stack>
+          {/* APP BAR TEXT */}
+          <AppBarText/>
+        </Toolbar>
+      </AppBar>
 
-        {/* COMPLEMENTARY IMAGE */}
-        <Box
-          component='img'
-          src={PictureComplementary}
-          alt=''
-          className={classes.pictureComplementary}
-        />
-      </Grid>
-
-      {/* MAIN CONTENT */}
-      <Grid
-        item
-        xs={6}
-        className={`${classes.content} ${classes.contentMain} zoom`}
+      {/* CHILDREN CONTAINER */}
+      <Stack 
+        className='zoom'
+        justifyContent='center'
+        alignItems='center'
+        flex='1'
       >
-        {/* PRODUCT LOGO */}
-        <Box
-          component='img'
-          src={LogoProductWithText}
-          alt=''
-        />
-
-        {/* CHILDREN CONTAINER */}
-        <Stack className={classes.containerChildren}>
+        <Stack className={classes.content}>
           {children}
         </Stack>
-
-        {/* EMPTY BOX */}
-        <Box/>
-      </Grid>
-    </Grid>
+      </Stack>
+    </Stack>
   )
 }
 
