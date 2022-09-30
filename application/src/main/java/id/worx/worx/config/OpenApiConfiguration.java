d7ad40a32cf5dfccc233aed6edf544bd23140f54
shygnome <shygnome3@gmail.com>
2022-09-30 14:43:32 +0700

    Add SecuredRestController interface

--- application/src/main/java/id/worx/worx/config/OpenApiConfiguration.java
+++ application/src/main/java/id/worx/worx/config/OpenApiConfiguration.java
@@ -4,12 +4,15 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class OpenApiConfiguration {
 
     @Bean
