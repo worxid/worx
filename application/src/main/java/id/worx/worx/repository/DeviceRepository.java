508df3a28e80119293d6b98e71a6f902efc3c3be
aliansyahkay <aliansyahkay@gmail.com>
2022-10-03 05:38:12 +0700

    add device join mobile

--- application/src/main/java/id/worx/worx/repository/DeviceRepository.java
+++ application/src/main/java/id/worx/worx/repository/DeviceRepository.java
@@ -8,6 +8,6 @@ public interface DeviceRepository extends BaseRepository<Devices, Long> {
 
     Optional<Devices> findByDeviceCode(String deviceCode);
     Optional<Devices> findByDeviceCodeAndDeleted(String deviceCode, boolean deleted);
-
     Optional<Devices> findByIdAndDeleted(Long deviceNo, boolean deleted);
+
 }
