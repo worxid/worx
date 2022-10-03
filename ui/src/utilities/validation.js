fec1b6a6c2ca4a2dd0c75a3148d2cc141127b324
jaballogian <jaballogianrma77@gmail.com>
2022-10-03 11:25:38 +0700

    add does object contains desired value function

--- ui/src/utilities/validation.js
+++ ui/src/utilities/validation.js
@@ -8,3 +8,11 @@ import {
 export const isObjectEmpty = (inputObject) => {
   return isEmpty(reject(equals(null), inputObject))
 }
+
+export const doesObjectContainDesiredValue = (inputObject, inputValue) => {
+  // MUST USE FILTER FUNCTION INSTEAD OF USING FIND FUNCTION
+  const resultList = Object.values(inputObject).filter(item => item === inputValue)
+  
+  if (resultList.length > 0) return true
+  else return false
+}
