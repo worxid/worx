6d7b45bc597ff1822e2094f2411dcccd62b2e160
jaballogian <jaballogianrma77@gmail.com>
2022-10-05 13:48:44 +0700

    add post create group function

--- ui/src/services/group.js
+++ ui/src/services/group.js
@@ -2,17 +2,35 @@
 import { axiosPrivate } from 'apis/axios'
 
 // TO DO: REPLACE WITH THE NEW API CONTAINING FILTER, SORT, PAGINATION, AND SEARCH
-export const getGroupList = async (
-  inputSignal, 
-  inputToken,
-) => {
+export const getGroupList = async (inputSignal) => {
   try {
     const response = await axiosPrivate.get(
       '/groups', 
       {
         signal: inputSignal,
-        //headers: { 'Authorization': `Bearer ${inputToken}` }
+        //headers: { 'Authorization': `Bearer ${inputToken}` },
       }
+    )
+
+    return response
+  } catch (error) {
+    if (!error.response) return { status: 'No Server Response' }
+    else return error.response
+  }
+}
+
+export const postCreateGroup = async (
+  inputSignal, 
+  inputBodyParams, 
+) => {
+  try {
+    const response = await axiosPrivate.post(
+      '/groups', 
+      inputBodyParams, 
+      {
+        signal: inputSignal,
+        //headers: { 'Authorization': `Bearer ${inputToken}` },
+      },
     )
 
     return response
