3d43224de1de71dae91a4e72b0d9aa4fdd51e4e5
dekguh <kdkteguhmahesa@gmail.com>
2022-10-11 10:41:37 +0800

    create form date validation search

--- ui/src/utilities/validation.js
+++ ui/src/utilities/validation.js
@@ -21,3 +21,13 @@ export const didSuccessfullyCallTheApi = (inputStatus) => {
   const successRegex = /^20[0-9]$/
   return successRegex.test(inputStatus)
 }
+
+/**
+ * 
+ * @param {string} date ex: 9-20-2022 15:00:00
+ */
+export const isFormatDateSearchValid = (date) => {
+  const pattern = /\d{1,2}\-\d{1,2}\-\d{4} \d{2}\:\d{2}\:\d{2}/g
+  if(pattern.test(date)) return true
+  else return false
+}
