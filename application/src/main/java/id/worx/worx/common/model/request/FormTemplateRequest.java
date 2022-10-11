e70594aacdd0dabaae5a546c6bc3f6187842aa24
shygnome <shygnome3@gmail.com>
2022-10-11 15:20:05 +0700

    Remove unused import

--- application/src/main/java/id/worx/worx/common/model/request/FormTemplateRequest.java
+++ application/src/main/java/id/worx/worx/common/model/request/FormTemplateRequest.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
