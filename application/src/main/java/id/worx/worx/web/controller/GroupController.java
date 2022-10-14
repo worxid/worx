e51fcd4f8b53a758c72cf78f0e359eefe9fdea3a
mohisasarwono16 <89118559+mohisasarwono16@users.noreply.github.com>
2022-10-14 13:46:56 +0700

    Group search api (#99)
    * Group Search API
    
    * authenticate group endpoint
    
    Co-authored-by: shygnome <shygnome3@gmail.com>

--- application/src/main/java/id/worx/worx/web/controller/GroupController.java
+++ application/src/main/java/id/worx/worx/web/controller/GroupController.java
@@ -5,6 +5,11 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import id.worx.worx.common.model.response.BasePageResponse;
+import id.worx.worx.web.model.request.GroupSearchRequest;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -57,11 +62,20 @@ public class GroupController implements SecuredRestController {
                 .body(response);
     }
 
+    @PostMapping("search")
+    public ResponseEntity<Page<GroupDTO>> create(@RequestBody GroupSearchRequest searchRequest, @ParameterObject Pageable pageable) {
+        Page<Group> groups = groupService.searchGroup(searchRequest,pageable);
+        List<GroupDTO> dtos = groups.stream().map(groupService::toDTO).collect(Collectors.toList());
+        Page<GroupDTO> page= new BasePageResponse<>(dtos,groups.getPageable(),groups.getTotalElements());
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(page);
+    }
+
     @DeleteMapping
     public ResponseEntity<BaseResponse> delete(@RequestBody @Valid MultipleDeleteRequest request) {
         groupService.delete(request.getIds());
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .body(BaseResponse.builder().build());
+            .body(BaseResponse.builder().build());
     }
 
     @GetMapping("{id}")
