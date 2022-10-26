75d608783ecc51ea48aeb516749c7d8a2ed6274b
dekguh <kdkteguhmahesa@gmail.com>
2022-10-26 17:41:21 +0800

    validation file function helper

--- ui/src/pages/FillForm/fillFormConstants.js
+++ ui/src/pages/FillForm/fillFormConstants.js
@@ -1,17 +1,21 @@
-// next to-do: compatible for upload input
+export const anyFormatFile = ['csv', 'doc', 'pdf', 'xls']
+export const anyFormatImage = ['jpg', 'jpeg', 'png']
+
 export const getKeyValue = (type) => {
   if(type === 'text' || type === 'date' || type === 'rating') return 'value'
   else if (type === 'checkbox_group') return  'values'
   else if (type === 'radio_group' || type === 'dropdown') return 'value_index'
+  else if (type === 'file' || type === 'photo') return  'values'
+  else if (type === 'signature') return  'value'
   else return 'value'
 }
 
-// next to-do: compatible for upload input
 export const getKeyValueType = (type, itemField) => {
-  if(type === 'text' || type === 'date') return ''
+  if(type === 'text' || type === 'date' || type === 'signature') return ''
   else if (type === 'checkbox_group') return itemField.group.map(item => false)
   else if (type === 'radio_group' || type === 'dropdown') return ''
   else if(type === 'rating') return 0
+  else if(type === 'file' || type === 'photo') return []
   else return ''
 }
 
@@ -28,7 +32,6 @@ export const structureParamsValuesCheckbox = (fields) => {
   return tempObject
 }
 
-// to-do: next make for upload input
 export const structureErrorMessage = (fields) => {
   let tempObject = {}
   fields.forEach(item => {
@@ -54,4 +57,46 @@ export const checkboxErrorMessage = (message, min_checked, max_checked) => {
   if(message.includes('Value is more than maximum')) return `Select maximum ${max_checked} option${max_checked >= 2 ? 's' : ''}`
   if(message.includes('Value is less than minimum')) return `Select minimum ${min_checked} option${min_checked >= 2 ? 's' : ''}`
   else return message
+}
+
+// CONVERT DATA URL TO JS FILE OBJECT
+export const dataURLtoFileObject = (dataurl, filename) => {
+  var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n)
+  while(n--){
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, {type:mime})
+}
+
+// FORMAT FILE VALIDATION
+export const formatFileValidation = (fileObject, allowedExtensions = ['jpg', 'png', 'jpeg']) => {
+  let isValid = false
+  allowedExtensions.forEach(item => {
+    if(fileObject?.type?.includes(item)) isValid = true
+  })
+
+  return isValid
+}
+
+const convertToBytes = (size, type) => {
+  const types = ['BYTE', 'KB', 'MB', 'GB', 'TB']
+  const key = types.indexOf(type.toUpperCase())
+  
+  if (typeof key !== 'boolean') {
+    return  size * 1024 ** key
+  }
+}
+
+// SIZE FILE VALIDATION
+export const sizeFileValidation = (fileObject, minSize, minFormat = 'byte', maxSize, maxFormat = 'byte') => {
+  console.log({ fileObject, minSize, minFormat, maxSize, maxFormat })
+  let isValid = false
+  const minInBytes = convertToBytes(minSize, minFormat)
+  const maxInBytes = convertToBytes(maxSize, maxFormat)
+  const fileInBytes = fileObject.size
+  console.log({fileInBytes, minInBytes, maxInBytes })
+
+  if(fileInBytes >= minInBytes && fileInBytes <= maxInBytes) isValid = true
+  return isValid
 }
