1fc8244782c858baae7eae4d2c465dfb4237875f
aliansyahkay <aliansyahkay@gmail.com>
2022-10-31 01:46:03 +0700

    create api get file

--- application/src/main/java/id/worx/worx/service/storage/FileStorageService.java
+++ application/src/main/java/id/worx/worx/service/storage/FileStorageService.java
@@ -1,10 +1,16 @@
 package id.worx.worx.service.storage;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import id.worx.worx.common.model.response.UrlPresignedResponse;
+import id.worx.worx.web.model.request.FileRequestDTO;
+import io.minio.errors.*;
 
 public interface FileStorageService {
 
@@ -25,4 +31,6 @@ public interface FileStorageService {
         return String.format("%s/%s/%s", foldername.toLowerCase(), writeFormat.format(new Date()), filename);
     }
 
+    List<UrlPresignedResponse> getFiles(FileRequestDTO fileRequestDTO)
+        throws IOException, InvalidResponseException, InvalidKeyException, NoSuchAlgorithmException, ServerException, ErrorResponseException, XmlParserException, InsufficientDataException, InternalException;
 }
--- application/src/main/java/id/worx/worx/web/controller/MediaController.java
+++ application/src/main/java/id/worx/worx/web/controller/MediaController.java
@@ -1,15 +1,23 @@
 package id.worx.worx.web.controller;
 
+import id.worx.worx.common.model.dto.DeviceDTO;
+import id.worx.worx.common.model.response.BaseListResponse;
+import id.worx.worx.web.model.request.FileRequestDTO;
+import io.minio.errors.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import id.worx.worx.common.model.response.UrlPresignedResponse;
 import id.worx.worx.service.storage.FileStorageService;
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("media")
@@ -24,4 +32,13 @@ public class MediaController {
                 .body(storageService.getUploadUrl(filename));
     }
 
+    @PostMapping("files")
+    public ResponseEntity<BaseListResponse<UrlPresignedResponse>> getFiles(@RequestBody @Valid FileRequestDTO fileRequestDTO) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        List<UrlPresignedResponse> urlPresignedResponses = storageService.getFiles(fileRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(BaseListResponse.<UrlPresignedResponse>builder()
+            .list(urlPresignedResponses)
+            .build());
+    }
 }
