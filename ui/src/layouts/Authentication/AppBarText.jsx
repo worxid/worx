b2c264bb578506176fa897e37f10778cb9195387
dekguh <kdkteguhmahesa@gmail.com>
2022-11-09 13:33:30 +0800

    implement api resend confirmation email

--- ui/src/layouts/Authentication/AppBarText.jsx
+++ ui/src/layouts/Authentication/AppBarText.jsx
@@ -1,14 +1,59 @@
+import { useContext } from 'react'
 import { useLocation, useSearchParams } from 'react-router-dom'
+
+// CONTEXTS
+import { AllPagesContext } from 'contexts/AllPagesContext'
 
 // MUIS
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 
+// SERVICES
+import { postResendEmailConfirmation } from 'services/users'
+
+// UTILITIES
+import { didSuccessfullyCallTheApi } from 'utilities/validation'
+
 const AppBarText = () => {
   const location = useLocation()
   const [ searchParams ] = useSearchParams()
 
+  // CONTEXTS
+  const { setSnackbarObject } = useContext(AllPagesContext)
+
   const pageType = searchParams.get('type')
+  const email = searchParams.get('email')
+
+  // HANDLE RESEND CLICK
+  const handleResendClick = async (event, type) => {
+    event.preventDefault()
+    const abortController = new AbortController()
+
+    if(type === 'sign-up' && email) {
+      const response = await postResendEmailConfirmation(
+        abortController.signal,
+        {
+          email,
+        }
+      )
+
+      if(didSuccessfullyCallTheApi(response?.status)) {
+        setSnackbarObject({
+          open: true,
+          severity:'success',
+          title: '',
+          message: 'Successfully requested to resend confirmation email',
+        })
+      } else {
+        setSnackbarObject({
+          open: true,
+          severity:'error',
+          title: response?.data?.error?.status?.replaceAll('_', ' ') || '',
+          message: response?.data?.error?.message || 'Something gone wrong',
+        })
+      }
+    }
+  }
 
   if (
     location.pathname === '/sign-up' ||
@@ -63,6 +108,7 @@ const AppBarText = () => {
         href='#'
         underline='none'
         className='fontFamilySpaceMono fontWeight700'
+        onClick={(event) => handleResendClick(event, pageType)}
       >
         Resend
       </Link>
