468b1333ac28e38c2914fe04678ab7cad8d07265
shygnome <shygnome3@gmail.com>
2022-11-10 09:44:44 +0700

    Add JsonProperty

--- application/src/main/java/id/worx/worx/common/model/dto/GroupDTO.java
+++ application/src/main/java/id/worx/worx/common/model/dto/GroupDTO.java
@@ -22,6 +22,7 @@ public class GroupDTO implements Serializable {
     private Long id;
     private String name;
     private String color;
+    @JsonProperty("is_default")
     private boolean isDefault;
 
     @JsonProperty("form_count")
