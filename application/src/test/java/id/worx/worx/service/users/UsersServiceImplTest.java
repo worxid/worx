677ffa10014e9b4eb7f4b59e604a83152d4f2c30
shygnome <shygnome3@gmail.com>
2022-11-14 10:11:49 +0700

    Add create method test

---
+++ application/src/test/java/id/worx/worx/service/users/UsersServiceImplTest.java
@@ -0,0 +1,89 @@
+package id.worx.worx.service.users;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import id.worx.worx.common.model.request.users.UserRequest;
+import id.worx.worx.config.properties.WebProperties;
+import id.worx.worx.config.properties.WorxProperties;
+import id.worx.worx.entity.users.Users;
+import id.worx.worx.mapper.UsersMapper;
+import id.worx.worx.repository.EmailTokenRepository;
+import id.worx.worx.repository.GroupRepository;
+import id.worx.worx.repository.RefreshTokenRepository;
+import id.worx.worx.repository.UsersRepository;
+import id.worx.worx.service.EmailService;
+import id.worx.worx.util.JwtUtils;
+
+@ActiveProfiles({ "test" })
+@ExtendWith(MockitoExtension.class)
+class UsersServiceImplTest {
+
+    @Mock
+    WorxProperties worxProps;
+
+    @Mock
+    UsersRepository usersRepository;
+    @Mock
+    RefreshTokenRepository refreshTokenRepository;
+    @Mock
+    EmailTokenRepository emailTokenRepository;
+    @Mock
+    GroupRepository groupRepository;
+
+    @Mock
+    JwtUtils jwtUtils;
+    @Mock
+    EmailService emailService;
+    @Mock
+    UsersMapper usersMapper;
+
+    private UsersService usersService;
+
+    @BeforeEach
+    void init() {
+        usersService = new UsersServiceImpl(
+                usersRepository,
+                worxProps,
+                refreshTokenRepository,
+                emailTokenRepository,
+                groupRepository,
+                jwtUtils,
+                emailService,
+                usersMapper);
+    }
+
+    @Test
+    void givenUserRequest_whenCreateUser_thenReturn() {
+        UserRequest userRequest = new UserRequest();
+        userRequest.setFullname("My Fullname");
+        userRequest.setPassword("MyPassword123!");
+        userRequest.setEmail("email@example.com");
+        userRequest.setPhoneNo("628307560133");
+        userRequest.setCountry("Indonesia");
+        userRequest.setOrganizationName("My Organization");
+
+        WebProperties webProps = new WebProperties();
+        webProps.setEndpoint("http://localhost:3000");
+
+        when(worxProps.getWeb()).thenReturn(webProps);
+
+        when(usersRepository.save(any(Users.class))).thenAnswer(invoc -> {
+            Users user = (Users) invoc.getArguments()[0];
+            user.setId(1L);
+            return user;
+        });
+
+        Users user = usersService.createUser(userRequest);
+        verify(usersRepository, times(1)).save(user);
+    }
+}
