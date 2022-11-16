c91cc39f6d4c393460d13b5932d2facd0aa62071
jaballogian <jaballogianrma77@gmail.com>
2022-11-16 17:11:21 +0700

    replace the function from the form service with the function from the guest service

--- ui/src/services/guest.js
+++ ui/src/services/guest.js
@@ -1,6 +1,22 @@
 //APIS
 import axios from 'apis/axios'
 
+export const getReadFormTemplate = async (formCode, inputSignal) => {
+  try {
+    const response = await axios.get(
+      `/form/template/read?code=${formCode}`, 
+      { signal: inputSignal }
+    )
+
+    return response
+  } 
+  catch (error) {
+    if (error.message === 'canceled') return { status: 'Canceled' }
+    else if (!error.response) return { status: 'No Server Response' }
+    else return error.response
+  }
+}
+
 export const postSubmitFormSubmission = async (inputSignal, inputParams) => {
   try {
     const response = await axios.post(
@@ -10,7 +26,8 @@ export const postSubmitFormSubmission = async (inputSignal, inputParams) => {
     )
     
     return response
-  } catch (error) {
+  } 
+  catch (error) {
     if (error.message === 'canceled') return { status: 'Canceled' }
     else if (!error.response) return { status: 'No Server Response' }
     else return error.response
