157b436322a710d73af3dbb6b24ae6058198a8dc
jaballogian <jaballogianrma77@gmail.com>
2022-11-17 13:13:19 +0700

    add form filter Stack component, its children, and the data

--- ui/src/pages/Home/Filters/Filters.jsx
+++ ui/src/pages/Home/Filters/Filters.jsx
@@ -1,8 +1,18 @@
+// CONSTANTS
+import { dummyFormList } from './filtersConstants'
+
 // MUIS
+import Autocomplete from '@mui/material/Autocomplete'
+import Checkbox from '@mui/material/Checkbox'
 import FormControl from '@mui/material/FormControl'
 import InputAdornment from '@mui/material/InputAdornment'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import Stack from '@mui/material/Stack'
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
 // MUI ICONS
@@ -46,6 +56,55 @@ const Filters = () => {
             }
           />
         </FormControl>
+      </Stack>
+
+      {/* FORM FILTER */}
+      <Stack
+        direction='row'
+        alignItems='center'
+        spacing='8px'
+      >
+        {/* TEXT */}
+        <Typography
+          variant='body2'
+          color='text.secondary'
+        >
+          Filter by Form:
+        </Typography>
+
+        {/* AUTOCOMPLETE */}
+        <Autocomplete
+          multiple
+          options={dummyFormList}
+          disableCloseOnSelect
+          getOptionLabel={(option) => option.text}
+          // className={classes.formControlInput}
+          sx={{ width: '500px' }}
+          renderOption={(props, option, { selected }) => (
+            <ListItem {...props}>
+              <ListItemButton>
+                {/* ICON */}
+                <ListItemIcon>
+                  <Checkbox
+                    size='small'
+                    checked={true}
+                  />
+                </ListItemIcon>
+
+                {/* TEXT */}
+                <ListItemText primary={option.text}/>
+              </ListItemButton>
+            </ListItem>
+          )}
+          renderInput={(params) => (
+            <TextField 
+              {...params} 
+              label='' 
+              placeholder='' 
+              // className={classes.formControlInput}
+            />
+          )}
+        />
       </Stack>
     </Stack>
   )
