3ef40580e82bc82eed4a9838de5340b9065d58eb
jaballogian <jaballogianrma77@gmail.com>
2022-11-24 11:00:31 +0700

    add date range time picker Dialog component

--- ui/src/components/DataGridFilters/DataGridFilters.jsx
+++ ui/src/components/DataGridFilters/DataGridFilters.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 
+// COMPONENTS
+import DateRangeTimePicker from 'components/DateRangeTimePicker/DateRangeTimePicker'
+
 // CUSTOM COMPONENTS
 import CustomTooltip from 'components/Customs/CustomTooltip'
 
 // MUIS
+import Badge from '@mui/material/Badge'
 import Checkbox from '@mui/material/Checkbox'
+import Dialog from '@mui/material/Dialog'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
@@ -13,6 +18,7 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 // MUI ICONS
+import IconDateRange from '@mui/icons-material/DateRange'
 import IconDelete from '@mui/icons-material/Delete'
 import IconDownload from '@mui/icons-material/Download'
 import IconEdit from '@mui/icons-material/Edit'
@@ -21,6 +27,7 @@ import IconSettings from '@mui/icons-material/Settings'
 import IconShare from '@mui/icons-material/Share'
 
 // STYLES
+import useLayoutStyles from 'styles/layoutPrivate'
 import useStyles from './dataGridFiltersUseStyles'
 
 const DataGridFilters = (props) => {
@@ -29,6 +36,16 @@ const DataGridFilters = (props) => {
     columns,
     selectedColumnList,
     setSelectedColumnList,
+    // DATE RANGE TIME
+    dateRangeValue,
+    dateRangeDateFormat,
+    dateRangeTimeFormat,
+    isWithTimePicker,
+    isWithDateTimePicker,
+    isDateRangeTimePickerOpen, 
+    setIsDateRangeTimePickerOpen,
+    handleSelectDateRangePickerButtonClick,
+    handleCancelDateRangePickerButtonClick,
     // FILTER
     isFilterOn, 
     setIsFilterOn,
@@ -48,6 +65,7 @@ const DataGridFilters = (props) => {
     handleDownloadButtonClick,
   } = props
 
+  const layoutClasses = useLayoutStyles()
   const classes = useStyles()
 
   const [ columnsMenuAnchor, setColumnsMenuAnchor ] = useState(null)
@@ -71,6 +89,26 @@ const DataGridFilters = (props) => {
       >
         {contentTitle}
       </Typography>
+
+      {/* DATE RANGE TIME PICKER BUTTON */}
+      {isWithDateTimePicker && 
+      <CustomTooltip 
+        title='Filter by Date Ranges'
+        placement='top'
+      >
+        <IconButton 
+          className={classes.iconButton}
+          onClick={() => setIsDateRangeTimePickerOpen(true)}
+          size='small'
+        >
+          <Badge 
+            color={dateRangeValue[0] !== null && dateRangeValue[1] !== null ? 'primary' : 'default'} 
+            variant='dot'
+          >
+            <IconDateRange fontSize='small'/>
+          </Badge>
+        </IconButton>
+      </CustomTooltip>}
 
       {/* DOWNLOAD ROW ICON */}
       {isDownloadButtonEnabled &&
@@ -195,6 +233,22 @@ const DataGridFilters = (props) => {
             </MenuItem>
         ))}
       </Menu>
+
+      {/* DATE RANGE TIME PICKER DIALOG */}
+      <Dialog 
+        open={isDateRangeTimePickerOpen}
+        onClose={() => setIsDateRangeTimePickerOpen(false)} 
+        className={layoutClasses.dialogDateRangePicker}
+      >
+        <DateRangeTimePicker
+          value={dateRangeValue}
+          dateFormat={dateRangeDateFormat}
+          timeFormat={dateRangeTimeFormat}
+          isWithTimePicker={isWithTimePicker}
+          handleSelectButtonClick={handleSelectDateRangePickerButtonClick}
+          handleCancelButtonClick={handleCancelDateRangePickerButtonClick}
+        />
+      </Dialog>
     </Stack>
   )
 }
@@ -207,6 +261,10 @@ DataGridFilters.defaultProps = {
   isFilterOn: false,
   // TEXT
   contentTitle: '',
+  // DATE RANGE VALUE
+  dateRangeValue: [],
+  isDateRangeTimePickerOpen: false,
+  isWithDateTimePicker: false,
   // SHARE
   isShareButtonEnabled: false,
   // EDIT
@@ -227,6 +285,13 @@ DataGridFilters.propTypes = {
   setIsFilterOn: PropTypes.func.isRequired,
   // TEXT
   contentTitle: PropTypes.string.isRequired,
+  // DATE RANGE VALUE
+  dateRangeValue: PropTypes.array.isRequired,
+  isDateRangeTimePickerOpen: PropTypes.bool.isRequired,
+  isWithDateTimePicker: PropTypes.bool.isRequired,
+  setIsDateRangeTimePickerOpen: PropTypes.func.isRequired,
+  handleSelectDateRangePickerButtonClick: PropTypes.func.isRequired,
+  handleCancelDateRangePickerButtonClick: PropTypes.func.isRequired,
   // SHARE
   isShareButtonEnabled: PropTypes.bool,
   handleShareButtonClick: PropTypes.func,
