6ed7b3fbb684d65581721a9e684ac78928aa5e04
Lani Diana <ganeshabook2015@gmail.com>
2022-11-25 10:30:45 +0700

    Frontend features -> Adjust styling

--- ui/src/components/DialogConfirmation/DialogConfirmation.jsx
+++ ui/src/components/DialogConfirmation/DialogConfirmation.jsx
@@ -65,7 +65,7 @@ const DialogConfirmation = (props) => {
       <DialogActions>
         {/* CANCEL BUTTON */}
         <CustomDialogActionButton 
-          className={`${layoutClasses.dialogButton} ${layoutClasses.greyButton}`}
+          className={`${layoutClasses.dialogButton} ${layoutClasses.greyButton} fontWeight600`}
           onClick={onCancelButtonClick}
         >
           {cancelButtonText}
