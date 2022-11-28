5aafc94f57d60ca706ba86ff6c9b77c314b8c99a
jaballogian <jaballogianrma77@gmail.com>
2022-11-28 14:33:49 +0700

    add header App Bar component, its children, and the styles

--- ui/src/pages/FormsSubmissions/DialogMediasPreview/DialogMediasPreview.jsx
+++ ui/src/pages/FormsSubmissions/DialogMediasPreview/DialogMediasPreview.jsx
@@ -1,18 +1,35 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 // HOOKS
 import useAxiosPrivate from 'hooks/useAxiosPrivate'
 
 // MUIS
+import AppBar from '@mui/material/AppBar'
 import Dialog from '@mui/material/Dialog'
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+// MUI ICONS
+import IconClose from '@mui/icons-material/Close'
+import IconFileDownload from '@mui/icons-material/FileDownload'
 
 // SERVICES
 import { postDetailMediaFiles } from 'services/media'
 
+// STYLES
+import useStyles from './dialogMediasPreviewUseStyles'
+
 const DialogMediasPreview = (props) => {
   const { mediasPreviewObject, setMediasPreviewObject } = props
 
+  const classes = useStyles()
+
   const axiosPrivate = useAxiosPrivate()
+
+  const [ mediaList, setMediaList ] = useState([])
+  const [ activeStep, setActiveStep ] = useState(0)
 
   const loadMediaFilesData = async (inputAbortController, inputIsMounted) => {
     const resultMediaFilesData = await postDetailMediaFiles(
@@ -20,6 +37,10 @@ const DialogMediasPreview = (props) => {
       { file_ids: mediasPreviewObject.file_ids },
       axiosPrivate,
     )
+
+    if (resultMediaFilesData.status === 200 && inputIsMounted) {
+      setMediaList(resultMediaFilesData.data.list)
+    }
   }
 
   useEffect(() => {
@@ -40,7 +61,34 @@ const DialogMediasPreview = (props) => {
       open={Boolean(mediasPreviewObject)}
       onClose={() => setMediasPreviewObject(null)}
     >
-      DialogMediasPreview
+      {/* HEADER */}
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          {/* FILES COUNT */}
+          <Typography 
+            variant='subtitle1'
+            
+          >
+            {`${mediaList.length} ${mediasPreviewObject?.type}${mediaList.length > 1 ? 's' : ''} (${activeStep + 1}/${mediaList.length})`}
+          </Typography>
+
+          {/* MENU */}
+          <Stack 
+            direction='row'
+            spacing='8px'
+          >
+            {/* DOWNLOAD ICON */}
+            <IconButton>
+              <IconFileDownload/>
+            </IconButton>
+
+            {/* CLOSE ICON */}
+            <IconButton onClick={() => setMediasPreviewObject(null)}>
+              <IconClose/>
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
     </Dialog>
   )
 }
---
+++ ui/src/pages/FormsSubmissions/DialogMediasPreview/dialogMediasPreviewUseStyles.js
@@ -0,0 +1,17 @@
+// MUI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+    padding: 24,
+    marginBottom: 0,
+  },
+  toolbar: {
+    height: 48,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+}))
+
+export default useStyles
