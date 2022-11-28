b94bd40e19a4c34eda381c27961912d32e1b184e
shygnome <shygnome3@gmail.com>
2022-11-28 21:13:42 +0700

    Resolve conflict

--- application/src/main/java/id/worx/worx/service/dashboard/DashboardServiceImpl.java
+++ application/src/main/java/id/worx/worx/service/dashboard/DashboardServiceImpl.java
@@ -1,22 +1,19 @@
 package id.worx.worx.service.dashboard;
 
-import id.worx.worx.common.model.request.DashboardRequest;
-import id.worx.worx.data.dto.DashboardStat;
-import id.worx.worx.data.dto.DashboardStatDTO;
-import id.worx.worx.entity.devices.Device;
-import id.worx.worx.repository.DeviceRepository;
-import id.worx.worx.repository.FormRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
 import id.worx.worx.common.model.dto.DashboardStatMapDTO;
 import id.worx.worx.common.model.request.DashboardRequest;
+import id.worx.worx.data.dto.DashboardStat;
+import id.worx.worx.data.dto.DashboardStatDTO;
 import id.worx.worx.entity.Form;
 import id.worx.worx.entity.devices.Device;
 import id.worx.worx.exception.WorxErrorCode;
@@ -28,16 +25,7 @@ import id.worx.worx.service.AuthenticationContext;
 import id.worx.worx.service.specification.FormSpecification;
 import id.worx.worx.web.model.request.FormSubmissionSearchRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
@@ -47,6 +35,7 @@ public class DashboardServiceImpl implements DashboardService {
     private final FormRepository formRepository;
     private final AuthenticationContext authContext;
     private final FormMapper formMapper;
+
     @Override
     public List<DashboardStatDTO> getDashboardStat(LocalDate from, LocalDate to, DashboardRequest dashboardRequest) {
         String deviceCode = "";
@@ -57,7 +46,7 @@ public class DashboardServiceImpl implements DashboardService {
             deviceCode = deviceData.getDeviceCode();
         }
 
-        List<DashboardStat> dashboardStatList = formRepository.getDasboardStat(from, to,deviceCode,dashboardRequest.getFormId());
+        List<DashboardStat> dashboardStatList = formRepository.getDasboardStat(from, to,deviceCode,dashboardRequest.getTemplateId());
         List<DashboardStatDTO> response = new ArrayList<>();
 
         for (DashboardStat data:dashboardStatList){
@@ -106,5 +95,5 @@ public class DashboardServiceImpl implements DashboardService {
         Specification<Form> spec = specification.fromSearchRequest(request,authContext.getUsers().getId(),deviceCode);
         return formRepository.findAll(spec);
     }
-    
+
 }
--- application/src/main/java/id/worx/worx/web/controller/DashboardController.java
+++ application/src/main/java/id/worx/worx/web/controller/DashboardController.java
@@ -1,21 +1,25 @@
 package id.worx.worx.web.controller;
 
-import id.worx.worx.common.model.request.DashboardRequest;
-import id.worx.worx.common.model.response.BaseListResponse;
-import id.worx.worx.data.dto.DashboardStatDTO;
-import id.worx.worx.common.model.dto.DashboardStatMapDTO;
-import id.worx.worx.common.model.request.DashboardRequest;
-import id.worx.worx.common.model.response.BaseListResponse;
-import id.worx.worx.service.dashboard.DashboardService;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.List;
+import id.worx.worx.common.model.dto.DashboardStatMapDTO;
+import id.worx.worx.common.model.request.DashboardRequest;
+import id.worx.worx.common.model.response.BaseListResponse;
+import id.worx.worx.data.dto.DashboardStatDTO;
+import id.worx.worx.service.dashboard.DashboardService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("dashboard-stats")
@@ -23,26 +27,29 @@ import java.util.List;
 public class DashboardController implements SecuredRestController {
 
     private final DashboardService dashboardService;
-    
+
     @PostMapping
-    public ResponseEntity<BaseListResponse<DashboardStatDTO>> list(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-                                                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
-                                                                   @Valid @RequestBody DashboardRequest dashboardRequest) {
+    public ResponseEntity<BaseListResponse<DashboardStatDTO>> getStats(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+            @Valid @RequestBody DashboardRequest dashboardRequest) {
         List<DashboardStatDTO> dashboardStatLists = dashboardService.getDashboardStat(from, to, dashboardRequest);
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseListResponse.<DashboardStatDTO>builder()
-            .list(dashboardStatLists)
-            .build());
+                .list(dashboardStatLists)
+                .build());
     }
-}
+
     @PostMapping("stats-map")
-    public ResponseEntity<BaseListResponse<DashboardStatMapDTO>> list(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-                                                                      @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
-                                                                      @Valid @RequestBody DashboardRequest dashboardRequest) {
-        List<DashboardStatMapDTO> dashboardStatMapLists = dashboardService.getDashboardStatMap(from, to, dashboardRequest);
+    public ResponseEntity<BaseListResponse<DashboardStatMapDTO>> getStatsMap(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+            @Valid @RequestBody DashboardRequest dashboardRequest) {
+        List<DashboardStatMapDTO> dashboardStatMapLists = dashboardService.getDashboardStatMap(from, to,
+                dashboardRequest);
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseListResponse.<DashboardStatMapDTO>builder()
-            .list(dashboardStatMapLists)
-            .build());
+                .list(dashboardStatMapLists)
+                .build());
     }
 }
