3166d342e5cdd965f81e93e35592a0f964815a75
Lani Diana <ganeshabook2015@gmail.com>
2022-11-29 17:10:58 +0700

    Fix show data for pop up

--- ui/src/pages/Home/Map/PopUp.jsx
+++ ui/src/pages/Home/Map/PopUp.jsx
@@ -9,6 +9,9 @@ import IconInsertLink from '@mui/icons-material/InsertLink'
 import IconLocationOn from '@mui/icons-material/LocationOn'
 import IconPhoneAndroid from '@mui/icons-material/PhoneAndroid'
 
+// UTILITIES
+import { convertDate } from 'utilities/date'
+
 const Popup = (props) => {
   const { 
     markerData, 
@@ -21,10 +24,33 @@ const Popup = (props) => {
       icon: IconInsertLink,
       text: markerData.id ?? 'No data',
     },
+    {
+      type: 'text',
+      icon: IconPhoneAndroid,
+      text:  markerData?.source?.type ? markerData?.source?.type.replace('_', ' ') : 'No data',
+    },
+    {
+      type: 'text',
+      icon: IconAccessTime,
+      text: markerData?.submit_date ? convertDate(markerData?.submit_date) : 'No data',
+    },
+    {
+      type: 'text',
+      icon: IconLocationOn,
+      text: markerData?.submit_location?.address ?? 'No data',
+    },
   ]
 
   return (
     <Box className={classes.popUpContainer}>
+      {/* TITLE */}
+      <Typography 
+        variant='caption'
+        className={`${classes.popUpTitle} textCapitalize`}
+      >
+        {markerData?.source?.label ?? ''}
+      </Typography>
+
       {/* CONTENT */}
       {informationList.map((item, index) => (
         <Box
