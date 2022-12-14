ee1db34c92168704bdd86bb84a4bb095d7a0c2f9
jaballogian <jaballogianrma77@gmail.com>
2022-12-14 16:51:42 +0700

    simplify the service

--- ui/src/services/strapi/dashboard.js
+++ ui/src/services/strapi/dashboard.js
@@ -1,14 +1,18 @@
 // APIS
 import axios from 'apis/axiosStrapi'
 
-export const getSignInMetaData = async (inputSignal) => {
+export const getDashboardMetaData = async (
+  inputSignal,
+  inputEndpoint,
+) => {
   try {
     const response = await axios.get(
-      '/dashboard-sign-in?populate=*',
+      `/${inputEndpoint}?populate=*`,
       { signal: inputSignal },
     )
     return response
-  } catch (error) {
+  } 
+  catch (error) {
     if (error.message === 'canceled') return { status: 'Canceled' }
     else if (!error.response) return { status: 'No Server Response' }
     else return error.response
