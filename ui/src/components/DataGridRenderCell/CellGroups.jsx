150df1656400323b037143b5133a1d3c0c131186
dekguh <kdkteguhmahesa@gmail.com>
2023-02-13 13:23:51 +0800

    fix comma bug

--- ui/src/components/DataGridRenderCell/CellGroups.jsx
+++ ui/src/components/DataGridRenderCell/CellGroups.jsx
@@ -16,21 +16,19 @@ const CellGroups = (props) => {
   const layoutClasses = useLayoutStyles()
 
   const calculateAndCutValue = () => {
-    if(isColumnGroup && !wrapperRef.current) return ''
+    if(limitShowGroup && wrapperRef.current) {
+      const totalMaxString = wrapperRef.current?.offsetWidth / 9 // offsetWidth/9 = max limit string
+      const restructureVal = dataValue?.map(item => item.name.replace(/\,/g, ';'))
+      const dataCutted = isColumnGroup ? restructureVal?.toString()?.substring(0, totalMaxString)?.split(',')?.filter(nameGroup => {
+        const isValidName = dataValue?.find(itemCheck => itemCheck.name.replace(/\,/g, ';') === nameGroup)
+        return Boolean(isValidName)
+      }) : dataValue.map(item => item.name).slice(0, 2)
 
-    const totalMaxString = wrapperRef.current?.offsetWidth / 9 // offsetWidth/9 = max limit string
-    const restructureVal = dataValue?.map(item => item.name)
-    const dataCutted = restructureVal?.toString()?.substring(0, totalMaxString)?.split(',')?.filter(nameGroup => {
-      const isValidName = dataValue?.find(itemCheck => itemCheck.name === nameGroup)
-      return Boolean(isValidName)
-    })
+      const maxLimitValues = isColumnGroup ? dataCutted.length : 2
 
-    const maxLimitValues = isColumnGroup ? dataCutted.length : 2
-
-    return limitShowGroup
-      ? (<>
+      return (<>
         <Typography variant='inherit'>
-          {dataValue.length ? dataCutted.toString().replace(/\,/g, ', ') : 'Default'}&nbsp;
+          {dataValue.length ? dataCutted.toString().replace(/\,/g, ', ').replace(/\;/g, ',') : 'Default'}&nbsp;
         </Typography>
         {(limitShowGroup && dataValue.length > maxLimitValues) && (
           <Avatar className={layoutClasses.avatar} variant='square'>
@@ -38,12 +36,12 @@ const CellGroups = (props) => {
           </Avatar>
         )}
       </>)
-      : (<>
-        {dataValue.length >= 1
-          ? dataValue.map(item => item.name).toString().replace(/\,/g, ', ')
-          : 'Default'
-        }
-      </>)
+    } else return (<>
+      {dataValue.length >= 1
+        ? dataValue.map(item => item.name).toString().replace(/\,/g, ', ').replace(/\;/g, ',')
+        : 'Default'
+      }
+    </>)
   }
 
   return (
