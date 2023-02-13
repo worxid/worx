0b5ff2c2ee49e50d7dee18cac6b2fa5a21977557
jaballogian <jaballogianrma77@gmail.com>
2023-02-13 17:05:16 +0700

    change the content

--- ui/src/pages/Error/index.jsx
+++ ui/src/pages/Error/index.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom'
 
 // DEFAULT IMAGE
 import defaultImg from 'assets/images/pictures/errorImg.svg'
@@ -5,6 +6,7 @@ import defaultImg from 'assets/images/pictures/errorImg.svg'
 // MUIS
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
@@ -13,27 +15,56 @@ import useStyles from './errorUseStyles'
 
 const Error = () => {
   const classes = useStyles()
+
+  const location = useLocation()
+
+  let title = 'Oops! Something went wrong.'
+  let caption = `We’re sorry, the page you requested could not be found.
+  Please go `
+
+  if (location?.state?.code === 404) {
+    title = 'Not Found'
+    caption = `We’re sorry, the page you requested could not be found.
+    Please go `
+  }
+  
   return (
     <Container className={`${classes.mainContainer} containerMaxWidth1400`}>
-      <Stack alignItems={'center'} justifyContent={'center'} className={classes.root}>
+      <Stack 
+        alignItems='center' 
+        justifyContent='center' 
+        className={classes.root}
+      >
+        {/* IMAGE */}
         <Box
           component='img'
           src={defaultImg}
-          alt={'Error'}
+          alt='Error'
           className={`${classes.img} width100`}
         />
+
+        {/* TITLE */}
         <Typography 
           variant='h4'
           className={classes.title}
         >
-          Oops! Something went wrong.
+          {title}
         </Typography>
+
+        {/* CAPTION */}
         <Typography 
           variant='h5'
           className={classes.caption}
         >
-          We’re sorry, the page you requested could not be found.<br/>
-          Please go back to the homepage.
+          {/* CAPTION */}
+          {caption}
+
+          {/* HOMEPAGE LINK */}
+          to the&nbsp;
+          <Link href='/'>
+            homepage
+          </Link>
+          .
         </Typography>
       </Stack>
     </Container>
