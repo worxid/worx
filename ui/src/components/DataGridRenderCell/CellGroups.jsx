0293ad6686c9dd1698b0fa7aa6b8cd51a18ff11d
just kadek <kdkteguhmahesa@gmail.com>
2023-02-13 14:07:10 +0800

    Frontend revisions/columns groups value auto (#387)
    * column groups value length follows the column
    
    * fix comma bug

--- ui/src/components/DataGridRenderCell/CellGroups.jsx
+++ ui/src/components/DataGridRenderCell/CellGroups.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import PropTypes from 'prop-types'
 
 // MUIS
@@ -9,30 +10,43 @@ import Typography from '@mui/material/Typography'
 import useLayoutStyles from 'styles/layoutPrivate'
 
 const CellGroups = (props) => {
-  const { dataValue, limitShowGroup } = props
+  const { dataValue, limitShowGroup, isColumnGroup } = props
+  const wrapperRef = useRef()
 
   const layoutClasses = useLayoutStyles()
 
+  const calculateAndCutValue = () => {
+    if(limitShowGroup && wrapperRef.current) {
+      const totalMaxString = wrapperRef.current?.offsetWidth / 9 // offsetWidth/9 = max limit string
+      const restructureVal = dataValue?.map(item => item.name.replace(/\,/g, ';'))
+      const dataCutted = isColumnGroup ? restructureVal?.toString()?.substring(0, totalMaxString)?.split(',')?.filter(nameGroup => {
+        const isValidName = dataValue?.find(itemCheck => itemCheck.name.replace(/\,/g, ';') === nameGroup)
+        return Boolean(isValidName)
+      }) : dataValue.map(item => item.name).slice(0, 2)
+
+      const maxLimitValues = isColumnGroup ? dataCutted.length : 2
+
+      return (<>
+        <Typography variant='inherit'>
+          {dataValue.length ? dataCutted.toString().replace(/\,/g, ', ').replace(/\;/g, ',') : 'Default'}&nbsp;
+        </Typography>
+        {(limitShowGroup && dataValue.length > maxLimitValues) && (
+          <Avatar className={layoutClasses.avatar} variant='square'>
+            +{dataValue.length - maxLimitValues}
+          </Avatar>
+        )}
+      </>)
+    } else return (<>
+      {dataValue.length >= 1
+        ? dataValue.map(item => item.name).toString().replace(/\,/g, ', ').replace(/\;/g, ',')
+        : 'Default'
+      }
+    </>)
+  }
+
   return (
-    <Stack direction='row' alignItems={'center'}>
-      {limitShowGroup
-        ? (<>
-          <Typography variant='inherit'>
-            {dataValue[0]?.name || 'Default'}
-            {dataValue.length >= 2 && `, ${dataValue[1]?.name}`}&nbsp;
-          </Typography>
-          {(limitShowGroup && dataValue.length > 2) && (
-            <Avatar className={layoutClasses.avatar} variant='square'>
-              +{dataValue.length - 2}
-            </Avatar>
-          )}
-        </>)
-        : (<>
-          {dataValue.length >= 1
-            ? dataValue.map(item => item.name).toString().replace(/\,/g, ', ')
-            : 'Default'
-          }
-        </>)}
+    <Stack ref={wrapperRef} direction='row' alignItems={'center'} sx={{ width: '100%' }}>
+      {calculateAndCutValue()}
     </Stack>
   )
 }
@@ -40,11 +54,13 @@ const CellGroups = (props) => {
 CellGroups.defaultProps = {
   dataValue: [],
   limitShowGroup: true,
+  isColumnGroup: false,
 }
 
 CellGroups.propTypes = {
   dataValue: PropTypes.array.isRequired,
   limitShowGroup: PropTypes.bool,
+  isColumnGroup: PropTypes.bool,
 }
 
 export default CellGroups
