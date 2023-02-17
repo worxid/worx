81d7a69fc900edacf9f4ce0da8f10d45f1841f1e
budivalid <budi@val.id>
2023-02-17 09:24:13 +0700

    fix: type of join #385

--- application/src/main/java/id/worx/worx/repository/FormTemplateRepository.java
+++ application/src/main/java/id/worx/worx/repository/FormTemplateRepository.java
@@ -26,23 +26,24 @@ public interface FormTemplateRepository
 
     List<FormTemplate> findAllByUserId(Long userId);
 
-    @Query(value = "SELECT ft FROM FormTemplate ft " +
-        " INNER JOIN ft.forms f " +
-        " INNER JOIN ft.assignedGroups g " +
-        "WHERE (:userId IS NULL OR ft.userId = :userId) " +
-        " AND (:#{#search.label} IS NULL OR upper(ft.label) LIKE upper(concat('%', :#{#search.label}, '%'))) " +
-        " AND (:#{#search.description} IS NULL OR upper(ft.description) LIKE upper(concat('%', :#{#search.description}, '%'))) " +
-        " AND (:#{#search.createdOn} IS NULL OR ft.createdOn <= :#{#search.createdOn}) " +
-        " AND (:#{#search.modifiedOn} IS NULL OR ft.modifiedOn <= :#{#search.modifiedOn}) " +
-        " AND (:#{#search.from} IS NULL OR :#{#search.to} IS NULL OR ft.createdOn BETWEEN :#{#search.from} AND :#{#search.to}) " +
-        " AND (:#{#search.assignedGroups} IS NULL OR g.name IN :#{#search.assignedGroups}) " +
-        " AND (:#{#search.submissionCount} IS NULL OR size(ft.forms) = :#{#search.submissionCount}) " +
-        " AND (:#{#search.globalSearch} IS NULL OR (" +
-        "  upper(ft.label) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
-        "  OR upper(ft.description) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
-        "  OR upper(g.name) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
-        ")) " +
-        "GROUP BY ft.id, g.name")
+    @Query(
+        value = "SELECT DISTINCT ft FROM FormTemplate ft " +
+            " LEFT JOIN ft.forms f " +
+            " LEFT JOIN ft.assignedGroups g " +
+            "WHERE (:userId IS NULL OR ft.userId = :userId) " +
+            " AND (:#{#search.label} IS NULL OR upper(ft.label) LIKE upper(concat('%', :#{#search.label}, '%'))) " +
+            " AND (:#{#search.description} IS NULL OR upper(ft.description) LIKE upper(concat('%', :#{#search.description}, '%'))) " +
+            " AND (:#{#search.createdOn} IS NULL OR ft.createdOn <= :#{#search.createdOn}) " +
+            " AND (:#{#search.modifiedOn} IS NULL OR ft.modifiedOn <= :#{#search.modifiedOn}) " +
+            " AND (:#{#search.from} IS NULL OR :#{#search.to} IS NULL OR ft.createdOn BETWEEN :#{#search.from} AND :#{#search.to}) " +
+            " AND (:#{#search.assignedGroups} IS NULL OR g.name IN :#{#search.assignedGroups}) " +
+            " AND (:#{#search.submissionCount} IS NULL OR size(ft.forms) = :#{#search.submissionCount}) " +
+            " AND (:#{#search.globalSearch} IS NULL OR (" +
+            "  upper(ft.label) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
+            "  OR upper(ft.description) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
+            "  OR upper(g.name) LIKE upper(concat('%', :#{#search.globalSearch}, '%')) " +
+            ")) GROUP BY ft.id, g.name"
+    )
     Page<FormTemplate> searchFormTemplates(
         @Nullable Long userId,
         @Param("search") FormTemplateSearchRequest search,
