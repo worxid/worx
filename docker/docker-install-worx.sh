9319679bcfd8b43092a0de087b4c751a18c96a1a
shygnome <shygnome3@gmail.com>
2023-03-08 17:36:25 +0700

    Add nginx.conf

--- docker/docker-install-worx.sh
+++ docker/docker-install-worx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker \xE2\x9C\x94"
+    echo -e "Docker ${GREEN}done"
 else
     echo "docker: command not found"
     echo "Please make sure docker is installed in your machine"
@@ -9,7 +11,7 @@ else
 fi
 
 if [ -x "$(command -v docker-compose)" ]; then
-    echo "Docker Compose \xE2\x9C\x94"
+    echo "Docker Compose ${GREEN}done"
 else
     echo "docker-compose: command not found"
     echo "Please make sure docker-compose is installed in your machine"
@@ -18,7 +20,15 @@ fi
 
 # start minio
 
-echo "Installing object storage..."
+echo "Installing object storage ..."
+mkdir -p .docker/data1-1
+mkdir -p .docker/data1-2
+mkdir -p .docker/data2-1
+mkdir -p .docker/data2-2
+mkdir -p .docker/data3-1
+mkdir -p .docker/data3-2
+mkdir -p .docker/data4-1
+mkdir -p .docker/data4-2
 docker-compose -f minio.yml pull
 docker-compose -f minio.yml up -d
 
