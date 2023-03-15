e1437b5d48f127828744a1a4c76637493f16b3be
Yudistira Hanifmuti <shygnome3@gmail.com>
2023-03-15 14:53:53 +0700

    Add run script (#399)
    * add docker-install-worx.sh
    
    * Add nginx.conf
    
    * Add application installation steps
    
    * Add more worx-core.env
    
    * add start and stop script
    
    * Add docker image name
    
    * Add network to backend
    
    * Add remove orphans flag
    
    * Add .docker to ignore
    
    * Omit valid annotation
    
    * Add docker compose with build context
    
    * Add sleep to install step
    
    * Change docker compose to use with build
    
    * add dev profile on start
    
    * Add build flag to docker compose up command
    
    * expose spring jpa hibernate and profile to env

---
+++ docker/docker-stop.worx.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+if [ -x "$(command -v docker)" ]; then
+    echo -e "${GREEN}\xE2\x9C\x94${NC} Docker"
+else
+    echo "docker: command not found"
+    echo "Please make sure docker is installed in your machine"
+    exit 1
+fi
+
+if [ -x "$(command -v docker-compose)" ]; then
+    echo -e "${GREEN}\xE2\x9C\x94${NC} Docker Compose"
+else
+    echo "docker-compose: command not found"
+    echo "Please make sure docker-compose is installed in your machine"
+    exit 1
+fi
+
+echo "Stopping object storage ..."
+docker-compose -f minio.yml down -v --remove-orphans
+
+echo "Stopping application ..."
+
+if [[ -z "${WORX_NODE_DOCKER_NAME}" ]]; then
+    echo "Using default image for worx-core"
+    WORX_NODE_DOCKER_NAME="shygnome3/worx:latest"
+else
+    WORX_NODE_DOCKER_NAME="${WORX_NODE_DOCKER_NAME}"
+fi
+
+if [[ -z "${WEB_UI_DOCKER_NAME}" ]]; then
+    echo "Using default image for worx-ui"
+    WEB_UI_DOCKER_NAME="shygnome3/worx-web-ui:latest"
+else
+    WEB_UI_DOCKER_NAME="${WEB_UI_DOCKER_NAME}"
+fi
+
+export WORX_NODE_DOCKER_NAME
+export WEB_UI_DOCKER_NAME
+
+docker-compose -f docker-compose.build.yml down -v --remove-orphans
